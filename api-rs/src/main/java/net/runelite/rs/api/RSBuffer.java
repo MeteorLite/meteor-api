23f5b3ee5bec9d1f4936c472dad9ae6286d74faa
TheRealNull <therealnull@gmail.com>
2022-06-15 22:54:21 -0400

    api-rs: update mappings

--- api-rs/src/main/java/net/runelite/rs/api/RSBuffer.java
+++ api-rs/src/main/java/net/runelite/rs/api/RSBuffer.java
@@ -3,20 +3,10 @@ package net.runelite.rs.api;
 import net.runelite.api.Buffer;
 import net.runelite.mapping.Import;
 
+import static net.runelite.rs.api.PacketFields.*;
+
 public interface RSBuffer extends Buffer, RSNode
 {
-	public static final String WRITEINT = "writeInt";
-	public static final String WRITESHORTLE = "method7550";
-	public static final String WRITESHORTADD = "method7551";
-	public static final String WRITEBYTE = "writeByte";
-	public static final String WRITEINTME = "method7565";
-	public static final String WRITEINTIME = "writeIntME";
-	public static final String WRITEBYTEADD = "method7687";
-	public static final String WRITEBYTESUB = "method7596";
-	public static final String WRITEINTLE = "method7563";
-	public static final String WRITESHORTADDLE = "method7641";
-	public static final String WRITEBYTENEG = "method7542";
-	public static final String WRITESHORT = "writeShort";
 	@Import("array")
 	byte[] getPayload();
 
@@ -35,6 +25,9 @@ public interface RSBuffer extends Buffer, RSNode
 	@Import("readUnsignedShort")
 	int readUnsignedShort();
 
+	@Import("readMedium")
+	int readMedium();
+
 	@Import("readShort")
 	int readShort();
 
@@ -45,11 +38,11 @@ public interface RSBuffer extends Buffer, RSNode
 	String readStringCp1252NullTerminated();
 
 	//	@Import("writeBoolean")
-//	void writeBoolean(boolean i);
-//
-//	@Import("writeBuffer")
-//	void writeBuffer(Buffer i);
-//
+	//	void writeBoolean(boolean i);
+	//
+	//	@Import("writeBuffer")
+	//	void writeBuffer(Buffer i);
+	//
 	@Import(WRITEBYTE)
 	void writeByte(int i);
 	//
@@ -62,15 +55,15 @@ public interface RSBuffer extends Buffer, RSNode
 	@Import(WRITEBYTESUB)
 	void writeByteSub(int i);
 	//
-//	@Import("writeBytes")
-//	void writeBytes(byte[] var1, int var2, int var3);
-//
-//	@Import("writeCESU8")
-//	void writeCESU8(CharSequence i);
-//
-//	@Import("writeCrc")
-//	int writeCrc(int i);
-//
+	//	@Import("writeBytes")
+	//	void writeBytes(byte[] var1, int var2, int var3);
+	//
+	//	@Import("writeCESU8")
+	//	void writeCESU8(CharSequence i);
+	//
+	//	@Import("writeCrc")
+	//	int writeCrc(int i);
+	//
 	@Import(WRITEINT)
 	void writeInt(int i);
 	//
@@ -83,24 +76,24 @@ public interface RSBuffer extends Buffer, RSNode
 	@Import(WRITEINTME)
 	void writeIntME(int i);
 	//
-//	@Import("writeLengthByte")
-//	void writeLengthByte(int i);
-//
-//	@Import("writeLengthInt")
-//	void writeLengthInt(int i);
-//
-//	@Import("writeLengthShort")
-//	void writeLengthShort(int i);
-//
-//	@Import("writeLong")
-//	void writeLong(long i);
-//
-//	@Import("writeLongMedium")
-//	void writeLongMedium(long i);
-//
-//	@Import("writeMedium")
-//	void writeMedium(int i);
-//
+	//	@Import("writeLengthByte")
+	//	void writeLengthByte(int i);
+	//
+	//	@Import("writeLengthInt")
+	//	void writeLengthInt(int i);
+	//
+	//	@Import("writeLengthShort")
+	//	void writeLengthShort(int i);
+	//
+	//	@Import("writeLong")
+	//	void writeLong(long i);
+	//
+	//	@Import("writeLongMedium")
+	//	void writeLongMedium(long i);
+	//
+	//	@Import("writeMedium")
+	//	void writeMedium(int i);
+	//
 	@Import(WRITESHORT)
 	void writeShort(int i);
 	//
@@ -113,15 +106,15 @@ public interface RSBuffer extends Buffer, RSNode
 	@Import(WRITESHORTLE)
 	void writeShortLE(int i);
 	//
-//	@Import("writeSmartByteShort")
-//	void writeSmartByteShort(int i);
-//
+	//	@Import("writeSmartByteShort")
+	//	void writeSmartByteShort(int i);
+	//
 	@Import("writeStringCp1252NullCircumfixed")
 	void writeStringCp1252NullCircumfixed(String i);
 	//
 	@Import("writeStringCp1252NullTerminated")
 	void writeStringCp1252NullTerminated(String i);
-//
-//	@Import("writeVarInt")
-//	void writeVarInt(int i);
+	//
+	//	@Import("writeVarInt")
+	//	void writeVarInt(int i);
 }
